15ceaa399d9297c29d8ad857901314645693396a
Jay R. Wren <jrwren@xmtp.net>
2023-03-25 11:10:45 -0400

    shfmt

--- watch.sh
+++ watch.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
+# who needs https://github.com/cosmtrek/air ? not me.
 trap k SIGINT
 
 k() {
-    kill $PID
+	kill $PID
 }
 go build -o app .
-./app & PID=$!
-while inotifywait -q -r -e modify *.go
-do kill "$PID"
-    go build -o app .
-    ./app &
-    PID=$!
+./app &
+PID=$!
+while inotifywait -q -r -e modify *.go; do
+	kill "$PID"
+	go build -o app .
+	./app &
+	PID=$!
 done
